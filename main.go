adc7643e3b9bd32948cd467a8f0a2efb5557f7cd
andrew <andrewwhwang@gmail.com>
2019-03-12 21:41:04 -0400

    fixed prevCandidate not clearing

--- main.go
+++ main.go
@@ -150,57 +150,82 @@ func rollingHash(s *string, k, kmerInterval int) chan int {
 	}()
 	return hashChan
 }
-
 func extendTail(sr string, ref *string, srPos, refPos, k, thres, window int) int{
 	end := srPos + k
-	eList := make([]int, window)
-	var eChan chan int
+	offset := refPos - srPos
 
-	wordLen := min(len(sr) - (srPos + k), len(*ref) - (refPos + k))
-	srCrop := sr[srPos+k:srPos+k+wordLen]
-	refCrop := (*ref)[refPos+k : refPos+k+wordLen]
-
-	if wordLen < 64 {
-		eChan = lev.MyerDist(srCrop, refCrop)
-	} else {
-		eChan = lev.MyerDistDiag(srCrop, refCrop, 61)
-	}
-
-	for wordLen > 0 && eList[len(eList)-1] - eList[0] <= thres{
+	for i := srPos + k - window; i < len(sr) - window; i++{
+		eDist := lev.Distance(sr[i:i+window], (*ref)[i+offset:i+offset+window])
 		end++
-		wordLen--
-		eDist := <-eChan
-		eList = append(eList[1:], eDist)
+		if eDist > thres || i + offset + window >= len(*ref) - 1 {
+			break
+		}
 	}
-	// close(eChan)
 	return end
 }
 
 func extendHead(sr string, ref *string, srPos, refPos, k, thres, window int) int{
 	start := srPos
-	eList := make([]int, window)
-	var eChan chan int
+	offset := refPos - srPos
 
-	wordLen := min(srPos, refPos)
-	srCrop := reverse(sr[srPos-wordLen:srPos])
-	refCrop := reverse((*ref)[refPos-wordLen:refPos])
-
-	if wordLen < 64 {
-		eChan = lev.MyerDist(srCrop, refCrop)
-	} else {
-		eChan = lev.MyerDistDiag(srCrop, refCrop, 61)
+	for i := srPos; i >= 0; i--{
+		eDist := lev.Distance(sr[i:i+window], (*ref)[i+offset:i+offset+window])
+		start = i
+		if eDist > thres || i + offset <= 0 {
+			break
+		}
 	}
-
-	for wordLen > 0 && eList[len(eList)-1] - eList[0] <= thres{
-		start--
-		wordLen--
-		eDist := <-eChan
-		eList = append(eList[1:], eDist)
-	}
-	// close(eChan)
 	return start
 }
+// func extendHead(sr string, ref *string, srPos, refPos, k, thres, window int) int{
+// 	start := srPos
+// 	eList := make([]int, window)
+// 	var eChan chan int
 
+// 	wordLen := min(srPos, refPos)
+// 	srCrop := reverse(sr[srPos-wordLen:srPos])
+// 	refCrop := reverse((*ref)[refPos-wordLen:refPos])
+
+// 	if wordLen < 64 {
+// 		eChan = lev.MyerDist(srCrop, refCrop)
+// 	} else {
+// 		eChan = lev.MyerDistDiag(srCrop, refCrop, 61)
+// 	}
+
+// 	for wordLen > 0 && eList[len(eList)-1] - eList[0] <= thres{
+// 		start--
+// 		wordLen--
+// 		eDist := <-eChan
+// 		eList = append(eList[1:], eDist)
+// 	}
+// 	return start
+// }
+
+
+// func extendTail(sr string, ref *string, srPos, refPos, k, thres, window int) int{
+// 	end := srPos + k
+// 	eList := make([]int, window)
+// 	var eChan chan int
+
+// 	wordLen := min(len(sr) - (srPos + k), len(*ref) - (refPos + k))
+// 	srCrop := sr[srPos+k:srPos+k+wordLen]
+// 	refCrop := (*ref)[refPos+k : refPos+k+wordLen]
+
+// 	if wordLen < 64 {
+// 		eChan = lev.MyerDist(srCrop, refCrop)
+// 	} else {
+// 		eChan = lev.MyerDistDiag(srCrop, refCrop, 61)
+// 	}
+
+// 	for wordLen > 0 && eList[len(eList)-1] - eList[0] <= thres{
+// 		end++
+// 		wordLen--
+// 		eDist := <-eChan
+// 		eList = append(eList[1:], eDist)
+// 	}
+// 	// close(eChan)
+// 	return end
+// }
 
 //TODO: make the diagonal width offset dynamic
 // make a square matrix from ranging from [0 - sr head] and [sr tail - end]
@@ -249,7 +274,7 @@ func printAln(aln string, offset int) {
 func main() {
 	start := time.Now()
 	k, kmerInterval := 8, 4
-	window, thres := 8, 5
+	window, thres := 5, 3
 
 	//make radix tree of short reads
 	//TODO: parallelize
@@ -280,7 +305,7 @@ func main() {
 				}
 			}
 		} else {
-			
+			prevCandidates = nil
 			//only run through process if > 5% different from last SR
 			for hash := range rollingHash(&sr, k, kmerInterval) {
 				if in, _ := bloom.GetBit(uint64(hash)); !in{ 
